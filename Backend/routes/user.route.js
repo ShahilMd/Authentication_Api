7bd30e4408fd1d698edc0610b4d44bf525d60bdb
ShahilMd <shahil.connect@gmail.com>
2025-11-11 13:49:29 +0530

    swagger api document update

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -29,11 +29,11 @@ const userRouter = express.Router()
 
 /**
  * @swagger
- * /auth/register:
+ * /api/v1/auth/register:
  *   post:
  *     summary: Register a new user and send verification email
  *     description: This endpoint registers a new user, validates input, sends a verification email, and applies rate limiting.
- *     tags: [Authentication]
+ *     tags: [Auth]
  *     requestBody:
  *       required: true
  *       content:
@@ -103,9 +103,10 @@ userRouter.post('/register', registerUser);
 
 /**
  * @swagger
- * /api/user/register/verify/{token}:
+ * /api/v1/register/verify/{token}:
  *   post:
  *     summary: Verify a user's email with token
+ *     description: The API endpoint is used to verify a user's email by accessing the verification token (token) included in the path variable.        When the user clicks on the email verification link provided to them, this API is invoked to validate their email address.  By including the verification token in the URL path, the endpoint confirms the authenticity of the token and marks the user's email as verified.
  *     tags: [Auth]
  *     parameters:
  *       - in: path
@@ -124,11 +125,10 @@ userRouter.post('/register/verify/:token', verifyUser);
 
 /**
  * @swagger
- * /api/user/login:
+ * /api/v1/login:
  *   post:
- *     summary: Login user and send OTP to email
- *     tags: [Auth]
- *     requestBody:
+ *     summary: Login user and send OTP to email.
+ *     description: This API endpoint check user cradentials if it was correct then send otp to the user mail id and apply ratelimiting 
  *       required: true
  *       content:
  *         application/json:
@@ -151,9 +151,9 @@ userRouter.post('/login', loginUser);
 
 /**
  * @swagger
- * /api/user/login/verify/otp:
+ * /api/v1/login/verify/otp:
  *   post:
- *     summary: Verify OTP and log in user
+ *     summary: Verify OTP and log in user. 
  *     tags: [Auth]
  *     requestBody:
  *       required: true
@@ -178,7 +178,7 @@ userRouter.post('/login/verify/otp', verifyOtp);
 
 /**
  * @swagger
- * /api/user/login/verify/resend/otp:
+ * /api/v1/login/verify/resend/otp:
  *   post:
  *     summary: Resend OTP to user email
  *     tags: [Auth]
@@ -200,7 +200,7 @@ userRouter.post('/login/verify/resend/otp', reSendOtp);
 
 /**
  * @swagger
- * /api/user/profile:
+ * /api/v1/profile:
  *   get:
  *     summary: Get current user profile
  *     tags: [Auth]
@@ -216,7 +216,7 @@ userRouter.get('/profile', isAuthenticated, profile);
 
 /**
  * @swagger
- * /api/user/profile/edit:
+ * /api/v1/profile/edit:
  *   post:
  *     summary: Edit user profile (name, profile image)
  *     tags: [Auth]
@@ -245,7 +245,7 @@ userRouter.post('/profile/edit', isAuthenticated, upload, editProfile);
 
 /**
  * @swagger
- * /api/user/profile/change/password:
+ * /api/v1/profile/change/password:
  *   post:
  *     summary: Change user password
  *     tags: [Auth]
@@ -274,7 +274,7 @@ userRouter.post('/profile/change/password', isAuthenticated, changePassword);
 
 /**
  * @swagger
- * /api/user/refresh:
+ * /api/v1/refresh:
  *   post:
  *     summary: Refresh access token using refresh token
  *     tags: [Auth]
@@ -288,7 +288,7 @@ userRouter.post('/refresh', refreshToken);
 
 /**
  * @swagger
- * /api/user/logout:
+ * /api/v1/logout:
  *   post:
  *     summary: Logout user and clear tokens
  *     tags: [Auth]
@@ -304,7 +304,7 @@ userRouter.post('/logout', isAuthenticated, verifyCsrfToken, logout);
 
 /**
  * @swagger
- * /api/user/refresh/csrf:
+ * /api/v1/refresh/csrf:
  *   post:
  *     summary: Refresh CSRF token for authenticated user
  *     tags: [Auth]
